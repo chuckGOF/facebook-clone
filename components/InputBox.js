c87120324adc6509ea920eec05b61e9fe2bf8bb3
Fadeel Gbaiye <chuck007gof@gmail.com>
2022-02-06 13:05:46 +0000

    storycard complete

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import Image from "next/image";
+import React from "react";
+import { userDetails } from "../constants";
 
 function InputBox() {
-  return <div></div>;
+	return (
+		<div className="bg-white-500 border mt-5">
+			<Image
+				src={userDetails.img}
+				alt=""
+				width={30}
+				height={30}
+				objectFit="contain"
+			/>
+			<form>
+				<input
+					type="text"
+					placeholder="What's on your mind Fadeel Gbaiye?"
+				/>
+			</form>
+      <button type='submit' hidden>Post</button>
+		</div>
+	);
 }
 
 export default InputBox;
